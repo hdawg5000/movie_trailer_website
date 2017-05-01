2f2676bf037f8a76527bed7c2e9b794866f48a3e
Hammad Naseem <hammadnsm@gmail.com>
2017-05-01 13:42:39 -0500

    Code cleanup

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Movie Trailer Website
 
 Using instances of the Movie class, a python script generates a static HTML page that lists poster art of my favorite movies. Clicking on the poster art will reveal a trailer for that movie.
+
+To open the movie trailer webpage, run the entertainment_center.py file
--- entertainment_center.py
+++ entertainment_center.py
@@ -2,15 +2,28 @@
 import movie
 import fresh_tomatoes
 
-#Create instances of the Movie class, passing in movie title, box art URL and movie trailer URL
-the_kite_runner = movie.Movie("The Kite Runner", "http://www.bdsfbd.com/wp-content/uploads/2016/05/the-kite-runner-movie.jpg", "https://www.youtube.com/watch?v=muW9Ob7V7ME")
-split = movie.Movie("Split", "https://images-na.ssl-images-amazon.com/images/M/MV5BZTJiNGM2NjItNDRiYy00ZjY0LTgwNTItZDBmZGRlODQ4YThkL2ltYWdlXkEyXkFqcGdeQXVyMjY5ODI4NDk@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://www.youtube.com/watch?v=84TouqfIsiI")
-the_dark_knight = movie.Movie("The Dark Knight", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UY1200_CR90,0,630,1200_AL_.jpg", "https://www.youtube.com/watch?v=EXeTwQWrcwY")
-fury = movie.Movie("Fury", "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA4MDU0NTUyN15BMl5BanBnXkFtZTgwMzQxMzY4MjE@._V1_UY1200_CR90,0,630,1200_AL_.jpg", "https://www.youtube.com/watch?v=-OGvZoIrXpg")
-lion = movie.Movie("Lion", "https://upload.wikimedia.org/wikipedia/en/f/f0/Lion_%282016_film%29.png", "https://www.youtube.com/watch?v=-RNI9o06vqo")
+"""Create instances of the Movie class, passing in movie title, box art URL and
+    movie trailer URL"""
+the_kite_runner = movie.Movie("The Kite Runner",
+    "http://www.bdsfbd.com/wp-content/uploads/2016/05/the-kite-runner-movie.jpg",
+    "https://www.youtube.com/watch?v=muW9Ob7V7ME")
+split = movie.Movie("Split",
+    "https://goo.gl/IGgEZj",
+    "https://www.youtube.com/watch?v=84TouqfIsiI")
+the_dark_knight = movie.Movie("The Dark Knight",
+    "https://goo.gl/S9zNFm",
+    "https://www.youtube.com/watch?v=EXeTwQWrcwY")
+fury = movie.Movie("Fury",
+    "https://goo.gl/bSzA8K",
+    "https://www.youtube.com/watch?v=-OGvZoIrXpg")
+lion = movie.Movie("Lion",
+    "https://upload.wikimedia.org/wikipedia/en/f/f0/Lion_%282016_film%29.png",
+    "https://www.youtube.com/watch?v=-RNI9o06vqo")
 
-#Store instances of Movie in a movies array, used by the fresh_tomatoes.py file to generate an HTML file
+"""Store instances of Movie in a movies array, used by the fresh_tomatoes.py
+    file to generate an HTML file"""
 movies = [the_kite_runner, split, the_dark_knight, fury, lion]
 
-#Call the open_movies method in the fresh_tomatoes file to generate the HTML page that displays the movies
+"""Call the open_movies method in the fresh_tomatoes file to generate the HTML
+    page that displays the movies"""
 fresh_tomatoes.open_movies_page(movies)
